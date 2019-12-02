1574c176450931238282a3d8ca38a0368f429cf5
skydragon <liliangchen1986@qq.com>
2019-12-02 18:21:42 +0800

    update new

---
+++ Exc6.4.py
@@ -0,0 +1,6 @@
+#6.4 Exc6.4 P163
+ls=[2,5,7,1,6]
+ls.sort()
+print(ls)
+ls.reverse()
+print(ls)
---
+++ Exc6.6.py
@@ -0,0 +1,9 @@
+#Exc6.6 P163
+ls1=[1,43]
+print(ls1)
+ls2=ls1
+print(ls1)
+print(ls2)
+ls1[0]=22
+print(ls1)
+print(ls2)
